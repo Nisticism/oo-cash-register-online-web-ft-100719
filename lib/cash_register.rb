4a9f6ec973b6fbe6426b590fae53ab96d7b0490b
Hans Foster <fosterhans@gmail.com>
2019-11-06 07:18:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,20 @@
 class CashRegister 
   
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0, discount = 0)
+    @total = total 
+    @discount = discount
+    
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price)
+    @total += 1 
+    
   
 end
 
