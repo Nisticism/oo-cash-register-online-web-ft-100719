84cba5837e16bc76eecb7ae1516eeb61b444ea99
Hans Foster <fosterhans@gmail.com>
2019-11-06 07:58:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,9 +37,7 @@ class CashRegister
   def void_last_transaction
     last_item = @item_prices.size - 1
     @total = @total - @item_prices[last_item]
-    if @item_prices.size == 0 
-      @total = 0.0 
-    end
+  
     @@items.pop
     @item_prices.pop
     if @item_prices.size == 0 
