dfa548d1179f2edc0d2aae5bbae3730928629c33
Hans Foster <fosterhans@gmail.com>
2019-11-06 07:26:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister 
   
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
   
   def initialize(total = 0, *discount)
     @total = total 
@@ -19,8 +19,17 @@ class CashRegister
   def apply_discount
     if @discount != nil 
       @total -= @discount
+      return "Success"
     else
+      return "There is no discount to apply."
+    end
   end
+  
+  def items
+    @items
+  end
+  
+  
   
   
   
